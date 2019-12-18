cf89fef4bb60283521f3e15a2caae2ea0fd7e93c
Ibcs16 <iagobrayhamcunha@hotmail.com>
2019-12-18 10:57:25 -0300

    update env

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './i18n';
 
+import dotenv from 'dotenv';
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+
+const envFile = process.env.NODE_ENV !== `test` ? `.env` : '.env.test';
+dotenv.config({ path: envFile });
 
 ReactDOM.render(
   <Suspense fallback={<div>Loading...</div>}>
