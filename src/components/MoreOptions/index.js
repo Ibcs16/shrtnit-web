c9e6d937d24bd61f686836d2ababb985714a2c7f
Ibcs16 <iagobrayhamcunha@hotmail.com>
2020-01-03 17:32:44 -0300

    Add labels to inputs, cause without them, it might confuse user

--- src/components/MoreOptions/index.js
+++ src/components/MoreOptions/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import { useTranslation } from 'react-i18next';
-import { IoMdCalendar, IoMdEye, IoMdEyeOff, IoMdLock } from 'react-icons/io';
-import { animated } from 'react-spring';
-
 import { Container, UrlOption, UrlOptionInput } from './styles';
+import { IoMdCalendar, IoMdEye, IoMdEyeOff, IoMdLock } from 'react-icons/io';
+import React, { useState } from 'react';
+
+import DatePicker from 'react-datepicker';
+import { animated } from 'react-spring';
+import { useTranslation } from 'react-i18next';
 
 export default function MoreOptions({
   handlePasswordChange,
@@ -23,57 +23,66 @@ export default function MoreOptions({
   // return component
   return (
     <animated.div
-      style={{ ...expandProps, width: '100%', marginBottom: '38px' }}
+      style={{ ...expandProps, width: '100%', marginBottom: '48px' }}
     >
+
       <Container>
-        <UrlOption>
-          <div>
-            <IoMdLock size={20} />
-          </div>
-          <UrlOptionInput
-            placeholder={t('translation:password')}
-            type={viewPassword ? 'text' : 'password'}
-            onChange={handlePasswordChange}
-          />
-          <div>
-            {!viewPassword ? (
-              <IoMdEye
-                onClick={() => setViewPassword(!viewPassword)}
-                size={20}
-                color="rgba(0,0,0,.3)"
-              />
-            ) : (
-              <IoMdEyeOff
-                onClick={() => setViewPassword(!viewPassword)}
-                size={20}
-                color="rgba(0,0,0,.3)"
-              />
-            )}
-          </div>
-        </UrlOption>
-        <UrlOption>
-          <div>
-            <IoMdCalendar size={20} />
-          </div>
+          <label
+            style={{
+              color: 'black',
+              opacity: .8
+            }}
+            htmlFor="access_key">
+            {t('translation:password')}
+            <UrlOption>
+              <div>
+                <IoMdLock size={20} />
+              </div>
+                <UrlOptionInput
+                  id="access_key"
+                  name="access_key"
+                  placeholder={t('translation:password_place_holder')}
+                  type={viewPassword ? 'text' : 'password'}
+                  onChange={handlePasswordChange}
+                />
+              <div>
+                {!viewPassword ? (
+                  <IoMdEye
+                  onClick={() => setViewPassword(!viewPassword)}
+                  size={20}
+                  color="rgba(0,0,0,.3)"
+                  />
+                  ) : (
+                    <IoMdEyeOff
+                    onClick={() => setViewPassword(!viewPassword)}
+                    size={20}
+                    color="rgba(0,0,0,.3)"
+                    />
+                    )}
+              </div>
+            </UrlOption>
+          </label>
+          <label
+            style={{
+              color: 'black',
+              opacity: .8
+            }}
+            htmlFor="expiration">
+              {t('translation:expiration')}
+            <UrlOption>
+              <div>
+                <IoMdCalendar size={20} />
+              </div>
 
-          {/* <DatePicker
-            style={{ zIndex: '99999' }}
-            locale={i18next.language}
-            selected={startDate}
-            onChange={date => setStartDate(date)}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            timeCaption="time"
-            dateFormat="MMMM d, yyyy h:mm aa"
-          /> */}
-
-          <UrlOptionInput
-            onChange={handleDateChange}
-            placeholder={t('translation:expiration')}
-            type="datetime-local"
-          />
-        </UrlOption>
+              <UrlOptionInput
+              name="expiration"
+              id="expiration"
+                onChange={handleDateChange}
+                // placeholder={t('translation:expiration')}
+                type="datetime-local"
+              />
+            </UrlOption>
+          </label>
       </Container>
     </animated.div>
   );
